d529035ed3b8d16685f611d99d5ed5ece6d68001
Sathriyansiva <sathriyan8@gmail.com>
2017-05-26 17:28:36 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app->get('/', function () {
+    return 'Hello World';
+});
+
+$app->get('/goodbye', function () {
+    return 'Goodbye World';
+});
+
+// @codeCoverageIgnoreStart
+if (PHP_SAPI != 'cli') {
+    $app->run();
+}
+// @codeCoverageIgnoreEnd
+
+return $app;
